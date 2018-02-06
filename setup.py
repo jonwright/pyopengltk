d1052c38a0f1faf3013da37c6966811aea39f5db
Jon Wright <wright@esrf.fr>
2018-02-06 13:42:19 +0100

    0.0.1 uploaded to pypi

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     name='pyopengltk',
     version='0.0.1',
     author ='Jon Wright',
+    author_email = 'jonathan.wright@gmail.com',
     url = 'http://github.com/jonwright/pyopengltk',
     py_modules=['pyopengltk'],
     long_description=open('README.md').read(),
