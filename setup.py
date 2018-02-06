26fd9d0fc3eb943c931b7b72feaf78beb78e24de
Jon Wright <wright@esrf.fr>
2018-02-06 11:29:07 +0100

    added a setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+
+from setuptools import setup
+# Try to get it to be pip installable
+setup(
+    name='pyopengltk',
+    version='0.0.1',
+    author ='Jon Wright',
+    url = 'http://github.com/jonwright/pyopengltk',
+    py_modules=['pyopengltk'],
+    long_description=open('README.md').read(),
+)
